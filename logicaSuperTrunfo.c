4ff3b8091e9c3a02654fc71b9d83330b68f47b2c
Wesley Melo <138056546+Rwtec@users.noreply.github.com>
2025-10-08 17:20:14 +0000

    Menu Interativo: Criado um menu interativo no terminal usando a estrutura switch que permita ao jogador escolher qual atributo será usado para comparar as cartas.
    Comparação de Atributos: Implementado a lógica de comparação entre duas cartas com base no atributo selecionado pelo jogador. Os atributos disponíveis são:
    
    Nome do país (string - usado apenas para exibir informações, não para comparação direta)
    
    População (int)
    
    Área (float)
    
    PIB (float)
    
    Número de pontos turísticos (int)
    
    Densidade demográfica (float - já calculada no desafio anterior).
    
    Regras de Comparação: A regra geral é: vence a carta com o maior valor no atributo escolhido.
    
    Exibição do Resultado: Mostrar na tela, de forma clara, o resultado da comparação, incluindo:
    
    O nome das duas cidades.
    O atributo usado na comparação.
    Os valores do atributo para cada carta.
    Qual carta venceu.
    Em caso de empate, exibindo a mensagem "Empate!".

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -75,49 +75,79 @@ int main(){
      printf("Digite o Numero de Pontos Turisticos: \n");
      scanf("%d", &Numero_de_Pontos_Turisticos2);
    
+<<<<<<< HEAD
     // ESCOLHA DO ATRIBUTO PARA COMPARAÇÃO
     // ESCOLHA AQUI QUAL ATRIBUTO COMPARAR:
     // 1 = População, 2 = Área, 3 = PIB, 4 = Densidade Populacional, 5 = PIB per Capita
     int atributo_escolhido = 4;  // Densidade Populacional (menor vence)
      
+=======
+
+    //MENU INTERATIVO
+    // menu interativo no terminal usando a estrutura switch que permita ao jogador escolher qual atributo será usado para comparar as cartas.
+    printf("*** Escolha o Atributo de comparação. ***\n");
+    printf("Sua escolha (1-6):\n");
+    printf("1.População.\n");
+    printf("2. Área.\n");
+    printf("3. PIB.\n");
+    printf("4. Númeoro de pontos turísticos.\n");
+    printf("5. Densidade demográfica.\n");
+    printf("6. PIB per capita. \n" );
+    scanf("%d", &escolhadojogador);
+
+   
+
+    switch (escolhadojogador)
+    {
+>>>>>>> cf7297c (Menu Interativo: Criado um menu interativo no terminal usando a estrutura switch que permita ao jogador escolher qual atributo será usado para comparar as cartas.)
     // COMPARAÇÕES ENTRE AS CARTAS
     // COMPARAÇÃO 1: POPULAÇÃO (MAIOR VENCE)
-    if(atributo_escolhido == 1){
+    case 1:
        printf("     Comparação de Cartas:População:\n\n");
        printf("Carta 1 - %s(%s):%d \n", Nome_da_Cidade1,Codigo1,Populacao1);
        printf("Carta 1 - %s(%s):%d \n", Nome_da_Cidade2,Codigo2,Populacao2);
-      if (Populacao1>Populacao2) {
+    if (Populacao1>Populacao2) {
         printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
       }else if (Populacao1>Populacao2) {
         printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
-     }
-   }
+      }else{
+        printf("Resultado:Empate!\n");
+      } break;
+      
+   
 
     // COMPARAÇÃO 2: ÁREA (MAIOR VENCE)
-    if(atributo_escolhido == 2){
+    case 2:
     printf("     Comparação de Cartas:Área:\n\n");
-    printf("Carta 1 - %s(%s):%.2f \n", Nome_da_Cidade1,Codigo1,Area1);
-    printf("Carta 2 - %s(%s):%.2f \n", Nome_da_Cidade2,Codigo2,Area2);
-     if (Area1>Area2) {
+    printf("Carta 1 - %s(%s):%.2f km² \n", Nome_da_Cidade1,Codigo1,Area1);
+    printf("Carta 2 - %s(%s):%.2f km²\n", Nome_da_Cidade2,Codigo2,Area2);
+    if (Area1>Area2) {
         printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
      }else if (Area1>Area2) {
         printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
-     }
-   }
+     }else{
+
+       printf("Resultado:Empate!\n");
+     } break;
+      
+   
 
      // COMPARAÇÃO 3: PIB (MAIOR VENCE)
-     if(atributo_escolhido == 3){
+     case 3 :
     printf("    Comparação de Cartas:PIB:\n\n");
-    printf("Carta 1 - %s(%s):%.2f\n", Nome_da_Cidade1,Codigo1,PIB1);
-    printf("Carta 2 - %s(%s):%.2f \n", Nome_da_Cidade2,Codigo2,PIB2);
+    printf("Carta 1 -  %s(%s):R$%.2f Bilhões.\n", Nome_da_Cidade1,Codigo1,PIB1);
+    printf("Carta 2 - %s(%s):R$%.2f Bilhões.\n", Nome_da_Cidade2,Codigo2,PIB2);
      if (PIB1>PIB2) {
         printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
      }else if (PIB1>PIB2){
         printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
-     }
-   }
+     }else{ 
+        printf("Resultado:Empate!\n");
+     } break;
+      
+   
     // COMPARAÇÃO 4: PONTOS TURÍSTICOS (MAIOR VENCE)
-    if(atributo_escolhido == 4){
+    case 4:
     printf("     Comparação de Cartas:Pontos Turísticos:\n\n");
     printf("Carta 1 - %s(%s):%d\n", Nome_da_Cidade1,Codigo1,Numero_de_Pontos_Turisticos1);
     printf("Carta 2 - %s(%s):%d \n", Nome_da_Cidade2,Codigo2,Numero_de_Pontos_Turisticos2);
@@ -125,37 +155,56 @@ int main(){
         printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
      }else if (Numero_de_Pontos_Turisticos1>Numero_de_Pontos_Turisticos2){
         printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
-     }
-   }
+     }else{  
+        printf("Resultado:Empate!\n");
+     } break;
+      
+   
     // COMPARAÇÃO 5: DENSIDADE POPULACIONAL (MENOR VENCE)
     // Calcula a densidade populacional: População / Área
     Densidade_Populacional1 = (Populacao1 / Area1);
     Densidade_Populacional2 = (Populacao2 / Area2);
 
-    if(atributo_escolhido == 5){
+    case 5:
        printf("      Comparação de Cartas:Densidade Populacional:\n\n");
-       printf("Carta 1 - %s(%s):%.2f \n", Nome_da_Cidade1,Codigo1,Densidade_Populacional1);
-       printf("Carta 2 - %s(%s):%.2f \n", Nome_da_Cidade2,Codigo2,Densidade_Populacional2);
-      if (Densidade_Populacional1<Densidade_Populacional2) {
+       printf("Carta 1 - %s(%s):%.2f hab/km²\n", Nome_da_Cidade1,Codigo1,Densidade_Populacional1);
+       printf("Carta 2 - %s(%s):%.2f hab/km²\n", Nome_da_Cidade2,Codigo2,Densidade_Populacional2);
+    if (Densidade_Populacional1<Densidade_Populacional2) {
           printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
-     }else{
+     }else if (Densidade_Populacional2 < Densidade_Populacional1){
           printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
-     }
-   }
+     }else {
+          printf("Resultado:Empate!\n");
+
+     } break;
+         
+   
     // // COMPARAÇÃO 6: PIB PER CAPITA (MAIOR VENCE)
     // Calcula o PIB per capita: PIB / População
     PIB_per_Capita1 = (PIB1 / Populacao1);
     PIB_per_Capita2 = (PIB2 / Populacao2);
+<<<<<<< HEAD
    if(atributo_escolhido == 1){
+=======
+
+   case 6:
+>>>>>>> cf7297c (Menu Interativo: Criado um menu interativo no terminal usando a estrutura switch que permita ao jogador escolher qual atributo será usado para comparar as cartas.)
       printf("       Comparação de Cartas:PIB per Capita:\n\n");
       printf("Carta 1 - %s(%s):%.2f\n", Nome_da_Cidade1,Codigo1,PIB_per_Capita1);
       printf("Carta 2 - %s(%s):%.2f \n", Nome_da_Cidade2,Codigo2,PIB_per_Capita2 );
-     if (PIB_per_Capita1>PIB_per_Capita2) {
+   if (PIB_per_Capita1>PIB_per_Capita2) {
         printf("Resultado :Carta 1(%s) Venceu!\n",Nome_da_Cidade1);
       }else if (PIB_per_Capita1>PIB_per_Capita2){
         printf("Resultado:Carta 2(%s) Venceu!\n",Nome_da_Cidade2);
+     }else{
+      printf("Resultado:Empate!\n");
      }
+     break;
+
+     default:
+            printf("Opcao invalida! Escolha entre 1 e 6.\n");
+            break;
+       
    }
-    
 return 0;
 }
