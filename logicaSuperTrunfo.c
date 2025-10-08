a80237b300e041cc85baa891191376e9b1b811c7
Wesley Melo <138056546+Rwtec@users.noreply.github.com>
2025-10-08 17:35:21 +0000

    push

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,7 +6,7 @@ int main(){
     char  Estado1 [50];                  // Nome do Estado (string)
     char  Codigo1 [50];                  // Código da carta (string)
     char  Nome_da_Cidade1 [50];          // Nome da cidade (string)
-    int  Populacao1;       // População (inteiro)
+    int  Populacao1;                     // População (inteiro)
     float Area1;                         // Área em km² (número decimal)
     float PIB1;                          // PIB em bilhões (número decimal)
     int   Numero_de_Pontos_Turisticos1;  // Número de pontos turísticos (inteiro)
